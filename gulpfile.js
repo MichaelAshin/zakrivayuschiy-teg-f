f7c4ffa556afb57b566e91b7a4e1bcdf0563dc71
Michael Ashin <ms@ash-in.ru>
2025-10-02 13:44:21 +0500

    homework_4_v2

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 // Подключаем плагины
-const gulp = require("gulp");
-const concat = require("gulp-concat-css");
-const plumber = require("gulp-plumber");
-const del = require("del");
-const browserSync = require("browser-sync").create();
+const gulp = require('gulp');
+const concat = require('gulp-concat-css');
+const plumber = require('gulp-plumber');
+const del = require('del');
+const browserSync = require('browser-sync').create();
 
 // Описываем задачу копирования HTML-файлов
 function html() {
   return gulp
-    .src("src/**/*.html")
+    .src('src/**/*.html')
     .pipe(plumber())
-    .pipe(gulp.dest("dist/"))
+    .pipe(gulp.dest('dist/'))
     .pipe(browserSync.reload({ stream: true }));
 }
 
@@ -19,10 +19,10 @@ exports.html = html;
 // Склеивание и сборка CSS
 function css() {
   return gulp
-    .src("src/styles/**/*.css")
+    .src('src/styles/**/*.css')
     .pipe(plumber())
-    .pipe(concat("styles/style.css"))
-    .pipe(gulp.dest("dist/"))
+    .pipe(concat('styles/style.css'))
+    .pipe(gulp.dest('dist/'))
     .pipe(browserSync.reload({ stream: true }));
 }
 
@@ -31,8 +31,8 @@ exports.css = css;
 // Изображения, шрифты, видео
 function images() {
   return gulp
-    .src("src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}", { encoding: false })
-    .pipe(gulp.dest("dist/images"))
+    .src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}', { encoding: false })
+    .pipe(gulp.dest('dist/images'))
     .pipe(browserSync.reload({ stream: true }));
 }
 
@@ -40,8 +40,8 @@ exports.images = images;
 
 function svgs() {
   return gulp
-    .src("src/svg/**/*.svg")
-    .pipe(gulp.dest("dist/svg"))
+    .src('src/svg/**/*.svg')
+    .pipe(gulp.dest('dist/svg'))
     .pipe(browserSync.reload({ stream: true }));
 }
 
@@ -49,8 +49,8 @@ exports.svgs = svgs;
 
 function fonts() {
   return gulp
-    .src("src/fonts/**/*.{woff,woff2,css}", { encoding: false })
-    .pipe(gulp.dest("dist/fonts"))
+    .src('src/fonts/**/*.{woff,woff2,css}', { encoding: false })
+    .pipe(gulp.dest('dist/fonts'))
     .pipe(browserSync.reload({ stream: true }));
 }
 
@@ -59,8 +59,8 @@ exports.fonts = fonts;
 // Скрипты js
 function scripts() {
   return gulp
-    .src("src/scripts/**/*.js")
-    .pipe(gulp.dest("dist/scripts"))
+    .src('src/scripts/**/*.js')
+    .pipe(gulp.dest('dist/scripts'))
     .pipe(browserSync.reload({ stream: true }));
 }
 
@@ -68,7 +68,7 @@ exports.scripts = scripts;
 
 // Очистка папки dist/
 function clean() {
-  return del("dist");
+  return del('dist');
 }
 
 // Сборка
@@ -76,19 +76,19 @@ exports.clean = clean;
 
 const build = gulp.series(
   clean,
-  gulp.parallel(html, css, images, svgs, fonts, scripts),
+  gulp.parallel(html, css, images, svgs, fonts, scripts)
 );
 
 exports.build = build;
 
 // Отслеживание изменений в файлах
 function watchFiles() {
-  gulp.watch(["src/**/*.html"], html);
-  gulp.watch(["src/blocks/**/*.css"], css);
-  gulp.watch(["src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}"], images);
-  gulp.watch(["src/svg/**/*.svg"], svgs);
-  gulp.watch(["src/fonts/**/*.{woff, woff2, css}"], fonts);
-  gulp.watch(["src/fonts/**/*.js"], scripts);
+  gulp.watch(['src/**/*.html'], html);
+  gulp.watch(['src/blocks/**/*.css'], css);
+  gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+  gulp.watch(['src/svg/**/*.svg'], svgs);
+  gulp.watch(['src/fonts/**/*.{woff, woff2, css}'], fonts);
+  gulp.watch(['src/fonts/**/*.js'], scripts);
 }
 const watchapp = gulp.parallel(build, watchFiles, serve);
 
@@ -98,7 +98,7 @@ exports.watchapp = watchapp;
 function serve() {
   browserSync.init({
     server: {
-      baseDir: "./dist",
+      baseDir: './dist',
     },
   });
 }
